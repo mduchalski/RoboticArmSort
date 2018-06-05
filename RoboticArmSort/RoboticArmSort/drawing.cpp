b039d353cc9a662a634b81c423ef2bbf60b786a1
Mateusz Duchalski <mduchalski1@gmail.com>
2018-06-05 10:59:24 +0200

    added magnet drawing and WM_SIZE handling

--- RoboticArmSort/RoboticArmSort/drawing.cpp
+++ RoboticArmSort/RoboticArmSort/drawing.cpp
@@ -13,3 +13,21 @@ void DrawWideCLine(Graphics& graphics, const Brush* brush, const PointF start,
 	graphics.FillEllipse(brush, RectF(stop.X - width / 2.0f,
 		stop.Y - width / 2.0f, width, width));
 }
+
+// Draws a "magnet" figure - straight surface of a given base length facing down
+// with smooth transition to a circle of a given mounting point (center) and
+// radius
+void DrawMagnet(Graphics& graphics, const Brush* brush, const PointF mount,
+	const REAL radius, const REAL height, const REAL base)
+{
+	graphics.FillEllipse(brush, RectF(mount.X - radius, mount.Y - radius,
+		2 * radius, 2 * radius));
+	REAL angle = (REAL)M_PI / 2.0f - atan(base / (2.0f * height)) - atan(
+		sqrt(height * height + base * base / 4.0f - radius * radius) / radius);
+	graphics.FillPolygon(brush, std::vector<PointF>{ 
+		PointF(mount.X - base / 2.0f, mount.Y + height),
+		PointF(mount.X + base / 2.0f, mount.Y + height),
+		PointF(mount.X + radius * cos(angle), mount.Y + radius * sin(angle)),
+		PointF(mount.X - radius * cos(angle), mount.Y + radius * sin(angle))
+	}.data(), 4);
+}
--- RoboticArmSort/RoboticArmSort/drawing.h
+++ RoboticArmSort/RoboticArmSort/drawing.h
@@ -5,5 +5,7 @@
 
 void DrawWideCLine(Graphics&, const Brush*, const PointF, const PointF, 
 	const REAL);
+void DrawMagnet(Graphics&, const Brush*, const PointF, const REAL, const REAL,
+	const REAL);
 
 #endif
