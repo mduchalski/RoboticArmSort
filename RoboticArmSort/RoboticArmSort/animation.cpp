238ffaed2e9b5ffd8258d50c63acc48dced9bb58
Mateusz Duchalski <mduchalski1@gmail.com>
2018-06-08 18:22:22 +0200

    reimplemented animation as object

--- RoboticArmSort/RoboticArmSort/animation.cpp
+++ RoboticArmSort/RoboticArmSort/animation.cpp
@@ -1,23 +1,103 @@
 #include "animation.h"
 #include "stdafx.h"
 #include "RoboticArmSort.h"
+#include "misc.h"
 
-// Alters move pair accordingly based on user key input
-bool DecodeKeys(std::pair<double, double>& move, const WPARAM keyId)
+Animation::Animation(const UINT _timerId, const double _maxSpeed)
 {
-	switch (keyId)
+	timerId = _timerId;
+	maxSpeed = _maxSpeed;
+
+	alfaMove = betaMove = 0.0;
+	isRunning = false;
+	target = { -1.0f, -1.0f };
+}
+
+void Animation::OnTick(HWND hWnd, Arm& _arm, const std::vector<Block>& _blocks)
+{
+	if (AutomaticMode())
+		UpdateSpeed();
+	
+	arm.Increment(alfaMove, betaMove);
+	if (InConflict(arm, blocks))
+	{
+		arm.Increment(-alfaMove, -betaMove);
+		KillTimer(hWnd, TMR);
+	}
+}
+
+void Animation::SetMaxSpeed(const double _maxSpeed)
+{
+	maxSpeed = _maxSpeed;
+}
+
+void Animation::OnKeydown(HWND hWnd, const WPARAM wParam)
+{
+	if (DecodeKeys(wParam))
+		UpdateSpeed();
+	else return;
+
+	if (!isRunning && !AutomaticMode())
+	{
+		SetTimer(hWnd, timerId, TIMER_FREQ, NULL);
+		isRunning = true;
+	}
+}
+
+void Animation::OnKeyup(HWND hWnd, const WPARAM wParam)
+{
+	alfaMove = (wParam == VK_LEFT || wParam == VK_RIGHT) ? 0.0 : alfaMove;
+	betaMove = (wParam == VK_UP || wParam == VK_DOWN) ? 0.0 : betaMove;
+
+	if (alfaMove == 0.0 && betaMove == 0.0)
+	{
+		isRunning = false;
+		KillTimer(hWnd, timerId);
+	}	
+}
+
+void Animation::GoToPos(const PointF _target)
+{
+	return;
+}
+
+void Animation::UpdateSpeed()
+{
+	if (AutomaticMode())
+	{
+
+	}
+	else
+	{
+		if (std::abs(alfaMove) > 10e-6)
+			alfaMove = (alfaMove > 0) ? maxSpeed / (double)(1000 / TIMER_FREQ) :
+				-maxSpeed / (double)(1000 / TIMER_FREQ);
+		if (std::abs(betaMove) > 10e-6)
+			betaMove = (betaMove > 0) ? maxSpeed / (double)(1000 / TIMER_FREQ) :
+			-maxSpeed / (double)(1000 / TIMER_FREQ);
+	}
+}
+
+bool Animation::AutomaticMode()
+{
+	return target.X > 0.0 || target.Y > 0.0;
+}
+
+bool Animation::DecodeKeys(const WPARAM wParam)
+{
+	switch (wParam)
 	{
 	case VK_LEFT:
-		move.first = ANIMATE_INCR;
+		alfaMove = 1.0;
 		break;
 	case VK_RIGHT:
-		move.first = -ANIMATE_INCR;
+		alfaMove = -1.0;
 		break;
 	case VK_UP:
-		move.second = ANIMATE_INCR;
+		betaMove = 1.0;
 		break;
 	case VK_DOWN:
-		move.second = -ANIMATE_INCR;
+		betaMove = -1.0;
 		break;
 	default:
 		return false;
--- RoboticArmSort/RoboticArmSort/animation.h
+++ RoboticArmSort/RoboticArmSort/animation.h
@@ -2,7 +2,35 @@
 #ifndef ANIMATION_H
 #define ANIMATION_H
 #include "stdafx.h"
+#include "Resource.h"
+#include "arm.h"
+#include "blocks.h"
 
-bool DecodeKeys(std::pair<double, double>&, const WPARAM);
+class Animation
+{
+public:
+	Animation(const UINT, const double);
+
+	// Basic actions and status checks
+	void OnTick(HWND, Arm&, const std::vector<Block>&);
+	void SetMaxSpeed(const double);
+
+	// Manual control actions
+	void OnKeydown(HWND, const WPARAM);
+	void OnKeyup(HWND, const WPARAM);
+
+	// Automatic constrol actions
+	void GoToPos(const PointF);
+
+private:
+	double alfaMove, betaMove, maxSpeed;
+	PointF target;
+	UINT timerId;
+	bool isRunning;
+
+	void UpdateSpeed();
+	bool AutomaticMode();
+	bool DecodeKeys(const WPARAM);
+};
 
 #endif
